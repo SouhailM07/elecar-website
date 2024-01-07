9af22a8c19ea01c6a0b4466fef01eeaed5714034
shadow <lightshadow416@gmail.com>
2024-01-07 03:56:58 +0100

    [Navbar] component checkpoint {1}

--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,6 +1,12 @@
 import "./mycontainer.css";
 // mui
 import { Container } from "@mui/material";
+// components
+import { Navbar } from "../../components";
 export default function MyContainer() {
-  return <>MyContainer</>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,36 @@
 import "./navbar.css";
+// mui
+import { AppBar, Toolbar, Stack } from "@mui/material";
+// assets
+import nav_logo from "/favicon.png";
 export default function Navbar() {
-  return <>Navbar</>;
+  let arrOfLinks: string[] = ["Home", "About", "Popular", "Features"];
+  return (
+    <>
+      <header>
+        <AppBar className="!bg-transparent" position="sticky">
+          <Toolbar className="justify-between !px-[7rem]">
+            <div className="flex items-center text-[1.5rem] space-x-2">
+              <img src={nav_logo} alt="logo" />
+              <p>Elecar</p>
+            </div>
+            <Stack
+              component="ul"
+              direction="row"
+              spacing="2.5rem"
+              className="font-medium text-[1.2rem]"
+            >
+              {arrOfLinks.map((e, i) => {
+                return (
+                  <li key={i}>
+                    <a href={`#${e}`}>{e}</a>
+                  </li>
+                );
+              })}
+            </Stack>
+          </Toolbar>
+        </AppBar>
+      </header>
+    </>
+  );
 }
