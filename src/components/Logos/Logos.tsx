b1fb1b29ed73f10683dc9757f13cfbc4be2050ac
shadow <lightshadow416@gmail.com>
2024-01-17 17:19:27 +0100

    [Logos] component has now [ScrollReveal] animation

--- src/components/Logos/Logos.tsx
+++ src/components/Logos/Logos.tsx
@@ -1,5 +1,7 @@
 import "./logos.css";
-// mui
+// hooks
+import { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 // assets
 import logo_1 from "/logo1.png";
 import logo_2 from "/logo2.png";
@@ -8,6 +10,15 @@ import logo_4 from "/logo4.png";
 import logo_5 from "/logo5.png";
 import logo_6 from "/logo6.png";
 export default function Logos() {
+  useEffect(() => {
+    const sr = ScrollReveal({
+      origin: "top",
+      distance: "60px",
+      duration: 2500,
+      delay: 400,
+    });
+    sr.reveal("#Logos li", { interval: 100 });
+  }, []);
   let arrOfLogos: string[] = [logo_1, logo_2, logo_3, logo_4, logo_5, logo_6];
   return (
     <>
