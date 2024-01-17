c410b48b962d2771176c13bccdb2addb55565f71
shadow <lightshadow416@gmail.com>
2024-01-17 16:29:17 +0100

    [Navbar] component is now RESPONSIVE

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,37 +1,51 @@
 import "./navbar.css";
 // mui
 import { AppBar, Toolbar, Stack } from "@mui/material";
+// components
+import { Navbar_mobile } from "../../components";
+// hooks
+import React, { useState } from "react";
+//@ts-ignore
+export const PanelContext = React.createContext();
 // assets
 import nav_logo from "/favicon.png";
+import menu_logo from "/bars-solid.svg";
 export default function Navbar() {
   let arrOfLinks: string[] = ["Home", "About", "Popular", "Features"];
+  let [openPanel, setOpenPanel] = useState<boolean>(false);
   return (
     <>
-      <AppBar
-        className="!bg-bodyColor !shadow-none m-auto max-w-[84rem]"
-        position="sticky"
-      >
-        <Toolbar className="justify-between ">
-          <div className="flex items-center text-[1.5rem] space-x-2">
-            <img src={nav_logo} alt="logo" />
-            <p>Elecar</p>
-          </div>
-          <Stack
-            component="ul"
-            direction="row"
-            spacing="2.5rem"
-            className="font-medium text-[1.2rem]"
-          >
-            {arrOfLinks.map((e, i) => {
-              return (
-                <li key={i}>
-                  <a href={`#${e}`}>{e}</a>
-                </li>
-              );
-            })}
-          </Stack>
-        </Toolbar>
-      </AppBar>
+      <PanelContext.Provider value={{ openPanel, setOpenPanel }}>
+        <AppBar
+          className="!bg-bodyColor !shadow-none m-auto max-w-[84rem]"
+          position="sticky"
+        >
+          <Toolbar className="justify-between items-center">
+            <div className="flex items-center text-[1.5rem] space-x-2">
+              <img src={nav_logo} alt="logo" />
+              <p>Elecar</p>
+            </div>
+            <button className="lg:hidden " onClick={() => setOpenPanel(true)}>
+              <img src={menu_logo} alt="logo" className="h-[1.5rem]" />
+            </button>
+            <Stack
+              component="ul"
+              direction="row"
+              spacing="2.5rem"
+              className="font-medium text-[1.2rem] lg:!flex !hidden"
+            >
+              {arrOfLinks.map((e, i) => {
+                return (
+                  <li key={i}>
+                    <a href={`#${e}`}>{e}</a>
+                  </li>
+                );
+              })}
+            </Stack>
+          </Toolbar>
+          <Navbar_mobile />
+        </AppBar>
+      </PanelContext.Provider>
     </>
   );
 }
--- src/components/Navbar_mobile/Navbar_mobile.tsx
+++ src/components/Navbar_mobile/Navbar_mobile.tsx
@@ -1,4 +1,38 @@
 import "./navbar_mobile.css";
+// mui
+import { Drawer } from "@mui/material";
+// hooks
+import { useContext } from "react";
+import { PanelContext } from "../Navbar/Navbar";
+// assets
+import close_logo from "/xmark-solid.svg";
 export default function Navbar_mobile() {
-  return <>Navbar_mobile</>;
+  let arrOfLinks: string[] = ["Home", "About", "Popular", "Features"];
+  let { openPanel, setOpenPanel }: any = useContext(PanelContext);
+  return (
+    <>
+      <Drawer
+        anchor="right"
+        open={openPanel}
+        // onClose={() => setOpenPanel(false)}
+      >
+        <div className="bg-gray-900 text-[1.3rem] h-full w-[100vw] overflow-hidden">
+          <div className="flex justify-end p-[2rem]">
+            <button onClick={() => setOpenPanel(false)}>
+              <img src={close_logo} alt="" className="h-[3rem] " />
+            </button>
+          </div>
+          <ul className="flex flex-col  justify-evenly text-white  h-[60%] font-bold text-center ">
+            {arrOfLinks.map((e, i) => {
+              return (
+                <li key={i} onClick={() => setOpenPanel(false)}>
+                  <a href={"#" + e}>{e}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Drawer>
+    </>
+  );
 }
