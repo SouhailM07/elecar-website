eeab02972e52da8e070697639db9ee1bbf4f0acc
shadow <lightshadow416@gmail.com>
2024-01-17 17:54:42 +0100

    removing unused lines in [Popular] component

--- src/components/Popular/Popular.tsx
+++ src/components/Popular/Popular.tsx
@@ -72,23 +72,6 @@ export default function Popular() {
       price: "175,900",
     },
   ];
-  const breakpoints = {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  };
   return (
     <>
       <article
