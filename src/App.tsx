de781169343ed72ec00a38a1ab6dea1423e9e189
shadow <lightshadow416@gmail.com>
2024-01-29 04:08:24 +0100

    adding [/Helmet/] component for increasing seo

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import "./style/input.css";
 import "./App.css";
 // components
 import { MyContainer } from "./components";
+//
+import { Helmet } from "react-helmet";
+
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="electric cars landing page" />
+        <meta
+          name="keywords"
+          content="responsive design, landing page, electric cars"
+        />
+        <meta name="author" content="Shadow" />
+      </Helmet>
       <MyContainer />
     </>
   );
